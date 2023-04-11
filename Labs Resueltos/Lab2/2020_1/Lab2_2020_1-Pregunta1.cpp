cb8fb6c9d8ebcef4537b44f2ce37691b04222561
DiegoIzaguirreLam <130415147+DiegoIzaguirreLam@users.noreply.github.com>
2023-04-10 23:18:29 -0500

    Update Lab2_2020_1-Pregunta1.cpp

--- Labs Resueltos/Lab2/2020_1/Lab2_2020_1-Pregunta1.cpp
+++ Labs Resueltos/Lab2/2020_1/Lab2_2020_1-Pregunta1.cpp
@@ -1,52 +1,29 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include <iostream>
-#define MAXCOL 10
+#define MAX 10
 
 using namespace std;
 
-int recorrido(int x, int y, int matriz[][MAXCOL], int L, int R, int T, int B){
-	int cuenta=0;
-	if(L==R or T==B){
-		cout<<matriz[T][L]<<endl;
-		return cuenta+1;
-	}
-	int i;
-	for(i=L; i<R; i++){
-		cout<<matriz[T][i]<<" ";
-		cuenta++;
-	}
-	//cout<<endl;
-	for(i=T; i<B; i++){
-		cout<<matriz[i][R]<<" ";
-		cuenta++;
-	}
-	//cout<<endl;
-	for(i=R; i>L; i--){
-		cout<<matriz[B][i]<<" ";
-		cuenta++;
-	} 
-	//cout<<endl;
-	for(i=B; i>T; i--){
-		cout<<matriz[i][L]<<" ";
-		cuenta++;
-	}
-	//cout<<endl;
-	cuenta=cuenta+recorrido(x+1, y+1, matriz, L+1, R-1, T+1, B-1);
-	return cuenta;
+void carga(int x, int y, int n, int m, int alma[][MAX]){
+	if(x>=n || y>=m) return;
+	if(x==n-1 && y==n-1) cout<<alma[x][y];
+	for(int i=y;i<m-1;i++) cout<<alma[x][i]<< " ";
+	for(int i=x;i<n-1;i++) cout<<alma[i][m-1]<< " ";
+	for(int i=m-1;i>y;i--) cout<<alma[n-1][i]<< " ";
+	for(int i=n-1;i>x;i--) cout<<alma[i][y]<< " ";
+	carga(x+1,y+1,n-1,m-1,alma);
 }
 
 int main(){
-	int n=5, m=5, matriz[][MAXCOL]={{1, 2, 3, 4, 5},
+	int alma[MAX][MAX]={             {1,  2,  3,  4, 5},
 									{16, 17, 18, 19, 6},
 									{15, 24, 25, 20, 7},
 									{14, 23, 22, 21, 8},
 									{13, 12, 11, 10, 9}};
-	int L=0, R=n-1, T=0, B=m-1;
-	cout<<"Total de monticulos encontrados: "<<recorrido(0, 0, matriz, L, R, T, B);
+	int n=5,m=5,cont=1;
+	
+	/*for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			alma[i][j]=cont++;*/
+	carga(0,0,n,m,alma);
 	return 0;
 }
-
-
