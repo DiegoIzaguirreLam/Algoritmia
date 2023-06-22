788fba1742f364617497b899221698e8f77c3ad9
DiegoIzaguirreLam <130415147+DiegoIzaguirreLam@users.noreply.github.com>
2023-06-22 00:43:46 -0500

    correcciones lab5

--- Labs Resueltos/Lab5/2022-2/Lab5 2022-2 Preg2.cpp
+++ Labs Resueltos/Lab5/2022-2/Lab5 2022-2 Preg2.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int x=0;
 int maxRect=0;
 int mov[8][2];
+int mrect[4][2];
 
 void cargamov(){
     mov[0][0]=-1; mov[0][1]=0;
@@ -17,19 +18,27 @@ void cargamov(){
     mov[7][0]=-1; mov[7][1]=-1;
 }
 
+void cargaRect(){
+    mrect[0][0]=-1; mrect[0][1]=0;
+    mrect[0][1]=0; mrect[1][1]=1;
+    mrect[0][2]=1; mrect[2][1]=0;
+    mrect[0][3]=0; mrect[3][1]=-1;
+}
+
 void imprimeTablero(int tablero[][8]){
     for(int i=0;i<8;i++){
         for(int j=0;j<8;j++)
             cout<<tablero[i][j]<<" ";
         cout<<endl;
     }
+    cout<<endl;
 }
 
 int posible(int n, int m,int c, int mapa[][MAX],int x,int y,int i){
 	int x1=x, y1=y;
 	for(int j=1; j<c; j++){
-		x1=x1+mov[i][0];
-		y1=y1+mov[i][1];
+		x1=x1+mrect[i][0];
+		y1=y1+mrect[i][1];
 		if(x1<0 || y1<0 || x1>=n || y1>=m) return 0;
 		if(mapa[x1][y1]!=0) return 0;
 	}
@@ -43,14 +52,14 @@ void rectangulos(int n,int m, int c,int mapa[][MAX], int x, int y, int cont){
     }
     if(cont>maxRect) maxRect=cont;
     int x1, y1;
-    mapa[x][y]=cont+1;//se pone el primero, se asume que esta en una posicion correcta
-    for(int i=0;i<8;i++){//para cada movimiento, se valida
-    	if(posible(n, m, c, mapa, x, y, i)){//si es posible poner un rectangulo en la direccion del movimiento
+    mapa[x][y]=cont+1;//se pone el primero, esta en una posicion correcta
+    for(int i=0;i<4;i++){//para cada direccion donde se puede generar un rectangulo, se valida
+    	if(posible(n, m, c, mapa, x, y, i)){//si es posible poner un rectangulo en la direccion del corte
 			x1=x;
 			y1=y;
     		for(int j=1;j<c;j++){//ubica el rectangulo
-    			x1=x1+mov[i][0];
-				y1=y1+mov[i][1];
+    			x1=x1+mrect[i][0];
+        		y1=y1+mrect[i][1];
 				mapa[x1][y1]=cont+1;
 			}
 			for(int j=0;j<8;j++){
@@ -58,22 +67,23 @@ void rectangulos(int n,int m, int c,int mapa[][MAX], int x, int y, int cont){
 			}
     		for(int j=1;j<c;j++){//borra el rectangulo
     			mapa[x1][y1]=0;
-    			x1=x1-mov[i][0];
-				y1=y1-mov[i][1];
+    			x1=x1-mrect[i][0];
+				y1=y1-mrect[i][1];
 			}
 		}
 	}
-	mapa[x][y]=0;
+	mapa[x][y]=0;//borra el primer cuadrado
 }
 
 int main()
 {
-    int n=4, m=4, c=2;
+    int n=5, m=5, c=3;
     int mapa[n][MAX];
     for(int i=0;i<n;i++)
         for(int j=0;j<m;j++)
             mapa[i][j]=0;
     cargamov();
+    cargaRect();
     rectangulos(n, m, c, mapa, 0, 0, 0);
     cout<<maxRect;
     
