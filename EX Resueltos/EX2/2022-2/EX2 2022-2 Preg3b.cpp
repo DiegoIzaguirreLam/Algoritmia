c412ef8cadf158a26414134f363e93da4ab46bad
DiegoIzaguirreLam <130415147+DiegoIzaguirreLam@users.noreply.github.com>
2023-07-12 00:38:19 -0500

    Update EX2 2022-2 Preg3b.cpp

--- EX Resueltos/EX2/2022-2/EX2 2022-2 Preg3b.cpp
+++ EX Resueltos/EX2/2022-2/EX2 2022-2 Preg3b.cpp
@@ -1,81 +1,64 @@
 
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #define MAX 10
 using namespace std;
-int solu[MAX];
-int y=0;
-int x=0;
-int nSol=0;
 
-void imprimeSolu(int pallet[][MAX],int M,int N){
-    for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++) cout<<pallet[i][j]<<" ";
-        cout<<endl;
-    }
-}
+int pallets[MAX][MAX]={0};
 
-int hallarpeso(int ind, int n, int peso, int paq[], int tomados[], int pallets[][MAX], int indPallet, int indPeso, int M, int N, int pesoObj){
-    if(indPallet==N){
-        imprimeSolu(pallets, M, N);
+int asignaPallets(int arr[],int tomados[], int N,int M,int n,int pesoObj, int indPeso, int iPallet,int iPeso, int peso){
+    if(iPallet==N){
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++)
+                cout<<pallets[i][j]<<" ";
+            cout<<endl;
+        }
         return 1;
     }
-    if(ind==n || peso<0 || indPeso==M)
-        return 0;
-
-    if(paq[ind]==peso && !tomados[ind]){//se termina de llenar el pallet y se pasa al siguiente pallet
-        pallets[indPallet][indPeso]=paq[ind];
-        tomados[ind]=1;
-        if(hallarpeso(0, n, pesoObj, paq, tomados, pallets, indPallet+1, 0, M, N, pesoObj)){
-            return 1;
+    if(peso<0 || indPeso==n) return 0;
+    if(!tomados[indPeso]){
+        if(peso==arr[indPeso]){//se completa y se pasa al siguiente pallet
+            tomados[indPeso]=1;
+            pallets[iPallet][iPeso]=arr[indPeso];
+            if(asignaPallets(arr, tomados, N, M, n, pesoObj, 0, iPallet+1, 0, pesoObj)){
+                return 1;
+            }
+            else{//back (de pallet)
+                tomados[indPeso]=0;
+                pallets[iPallet][iPeso]=0;
+                return asignaPallets(arr, tomados, N, M, n, pesoObj, indPeso+1, iPallet, iPeso, peso);
+            }
         }
-        else{
-            pallets[indPallet][indPeso]=0;
-            tomados[ind]=0;
-            return 0;
+        else{//se agrega el peso al pallet
+            tomados[indPeso]=1;
+            pallets[iPallet][iPeso]=arr[indPeso];
+            if(asignaPallets(arr, tomados, N, M, n, pesoObj, indPeso+1, iPallet, iPeso+1, peso-arr[indPeso])){
+                return 1;
+            }
+            else{//back (de peso colocado en pallet)
+                tomados[indPeso]=0;
+                pallets[iPallet][iPeso]=0;
+                return asignaPallets(arr, tomados, N, M, n, pesoObj,indPeso+1, iPallet, iPeso, peso);
+            }
         }
     }
-    if(!tomados[ind]){
-        pallets[indPallet][indPeso]=paq[ind];
-        tomados[ind]=1;
-        if(hallarpeso(ind+1, n, peso-paq[ind], paq, tomados, pallets, indPallet, indPeso+1, M, N, pesoObj)){
-            return 1;
-        }
-        else{
-            pallets[indPallet][indPeso]=0;
-            tomados[ind]=0;
-            return hallarpeso(ind+1, n, peso, paq, tomados, pallets, indPallet, indPeso, M, N, pesoObj);
-        }
+    else{//no se puede volver a tomar el peso, se pasa al siguiente
+        return asignaPallets(arr, tomados, N, M, n, pesoObj,indPeso+1, iPallet, iPeso, peso);
     }
-    else{
-        return hallarpeso(ind+1, n, peso, paq, tomados, pallets, indPallet, indPeso, M, N, pesoObj);
-    }
-}
-
-int calculaSuma(int arr[], int n){
-    int suma=0;
-    for(int i=0;i<n;i++) suma += arr[i];
-    return suma;
 }
 
 int main(){
-    int M = 2, N = 3;
+    int M=2, N=3;
     int arr[]={4, 5, 3, 1, 3, 2};
     int n=6;
-    int pallets[N][MAX]={};
     int tomados[n]={};
-    int suma = calculaSuma(arr, n);
-    if(n == M*N){
-        if(!hallarpeso(0, n, suma/N, arr, tomados, pallets, 0, 0, M, N, suma/N))
-            cout<<"No hay soluciones"<<endl;
-    }
-    else{
-        cout<<"La cantidad de pallets debe ser igual a N*M"<<endl;
+    int suma=0;
+    for(int i=0;i<n;i++) suma+=arr[i];
+    
+    if(!asignaPallets(arr, tomados, N, M, n, suma/N, 0, 0, 0, suma/N)){
+        cout<<"No se pudieron asignar los pesos equitativamente a los pallets"<<endl;
     }
     return 0;
 }
-
-
-
-
-
 
